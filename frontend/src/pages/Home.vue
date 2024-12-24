54bd68936fdb7428fe57b51bf1f969868abfbf73
kenta-afk <kentarou9.60@icloud.com>
2024-12-24 18:20:16 +0900

    修正

---
+++ frontend/src/pages/Home.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1>人狼殺</h1>
+    <button @click="register">register</button>
+    <button @click="login">login</button>
+</template>
+<script setup>
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const register = () => {
+    router.push('/register')
+}
+
+const login = () => {
+    router.push('/login')
+}
+</script>
+<style scoped>
+</style>
---
+++ frontend/src/pages/Top.vue
@@ -0,0 +1,19 @@
+<template>
+  <LogoutButton />
+</template>
+
+<script>
+import LogoutButton from '../components/LogoutButton.vue'
+
+export default {
+  components: {
+    LogoutButton
+  }
+}
+</script>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
